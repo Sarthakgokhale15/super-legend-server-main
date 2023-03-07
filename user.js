cdaa21edd623ad1c37e65fe7e44443f1320d1be2
VIPER_619 <64783930+Sarthakgokhale15@users.noreply.github.com>
2023-03-07 15:11:45 +0530

    sheet intgration done

--- user.js
+++ user.js
@@ -16,8 +16,7 @@ router.post("/test",async(req,res)=>{
     res.send("post working");
 })
 
-router.post("/user",async(req,res)=>{
-
+async function getAuthSheets(){
     const auth=new google.auth.GoogleAuth({
         credentials:{
             "type": "service_account",
@@ -33,24 +32,29 @@ router.post("/user",async(req,res)=>{
           },
         scopes:"https://www.googleapis.com/auth/spreadsheets"
     });
-
-    const client=  await auth.getClient;
+    const client=  await auth.getClient();
     const googleSheets=google.sheets({version:"v4",auth:client});
     const spreadsheetId="1zUeJVMWXURWAcz5UpadyiGjYWlnIpnB8Kgfd6F9k4Cs";
+    return {
+        auth,client,googleSheets,spreadsheetId
+    }
+}
+router.post("/user",async(req,res)=>{
+    const {auth,googleSheets,spreadsheetId}=await getAuthSheets();
+    
 
 
 
-    // console.log(req.body.body);
-    // console.log(req.query.childName);
-    // const jcaIdNumber=req.query.jcaIdNumber;
-    // console.log(jcaIdNumber);
+
+    console.log(req.body.body);
+
     const payload=[[req.body.body.childName,req.body.body.Age,req.body.body.gender,req.body.body.parentName,req.body.body.category1,req.body.body.category2,req.body.body.category3,req.body.body.ContactNumber,req.body.body.location,req.body.body.jcaId,req.body.body.jcaIdNumber,req.body.body.mentorName]];
-    // const payload=[[req.query.childName,req.query.Age,req.query.gender,req.query.parentName,req.query.category1,req.query.category2,req.query.category3,req.query.ContactNumber,req.query.location,req.query.jcaId,req.query.jcaIdNumber,req.query.mentorName]];
 
 
 
-    try{
-        await googleSheets.spreadsheets.values.append({
+
+
+        const row=await googleSheets.spreadsheets.values.append({
             auth,
             spreadsheetId,
             range:"Sheet1!A:B",
@@ -61,15 +65,10 @@ router.post("/user",async(req,res)=>{
     
     
         })
-    }
-    catch(e){
-        res.status(400).send(e);
-    }
+        res.send(row.data);
    
-    // console.log(done.status);
-    res.send("server running data posted sucessfully");
-    // return res.status(200);
-    // return res;
+   
+    
 
 });
 module.exports=router
