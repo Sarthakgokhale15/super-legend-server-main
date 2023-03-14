6fb3431ef4c0b985e3fc80dd408b48817585c7ac
VIPER_619 <64783930+Sarthakgokhale15@users.noreply.github.com>
2023-03-14 13:22:40 +0530

    email integration done

--- user.js
+++ user.js
@@ -1,4 +1,6 @@
 const express=require("express")
+const nodemailer=require("nodemailer")
+const MailGen=require("mailgen")
 const router=new express.Router(); 
 
 var cors = require('cors');
@@ -65,19 +67,40 @@ router.post("/partner",async(req,res)=>{
 });
 router.post("/user",async(req,res)=>{
     const {auth,googleSheets,spreadsheetId}=await getAuthSheets();
-    
+    const email=req.body.body.email;
+    const parentName=req.body.body.parentName;
 
 
 
 
     console.log(req.body.body);
 
-    const payload=[[req.body.body.childName,req.body.body.Age,req.body.body.gender,req.body.body.parentName,req.body.body.category1,req.body.body.category2,req.body.body.category3,req.body.body.FormattedContactNumber,req.body.body.location,req.body.body.JcaMember,req.body.body.cat1,req.body.body.cat2,req.body.body.cat3,req.body.body.cat4,req.body.body.cat5,req.body.body.cat6,req.body.body.cat7,req.body.body.cat8,req.body.body.cat9,req.body.body.cat10]];
+    const payload=[
+        [
+            req.body.body.childName,
+            req.body.body.Age,
+            req.body.body.gender,
+            req.body.body.parentName,
+            req.body.body.category1,
+            req.body.body.category2,
+            req.body.body.category3,
+            req.body.body.FormattedContactNumber,
+            req.body.body.location,
+            req.body.body.email,
+            req.body.body.cat1,
+            req.body.body.cat2,
+            req.body.body.cat3,
+            req.body.body.cat4,
+            req.body.body.cat5,
+            req.body.body.cat6,
+            req.body.body.cat7,
+            req.body.body.cat8,
+            req.body.body.cat9,
+            req.body.body.cat10,
+
+        ]];
     
-    if(payload[0][4]==true && payload[0][9]==''){
-        res.status(404).send('Data Not Stored');
-        return ;
-    }
+   
 
     if(payload[0][0]=='' || payload[0][1]=='' || payload[0][2]=='' || payload[0][3]==''|| payload [0][7]=='' || payload[0][8]=='' || payload[0][7].length!=10 ){
         res.status(404).send('Data Not Stored');
@@ -99,9 +122,59 @@ router.post("/user",async(req,res)=>{
     
     
         })
-        res.send(row.data);
+        // res.send(row.data);
     }
 
+
+        let config={
+            service:'gmail',
+            auth:{
+                user:process.env.EMAIL,
+                pass:process.env.PASSWORD
+            }
+        }
+        let transporter=nodemailer.createTransport(config)
+        let MailGenerator=new MailGen({
+            theme:"default",
+            product : {
+                name: "SuprLegend",
+                link : 'https://mailgen.js/'
+            }
+
+        })
+
+
+
+        let response={
+            body:{
+                name:parentName,
+                intro:"Your account created successfully!",
+                outro:"Looking forward to engage with you."
+            }
+        }
+
+
+      let mail=MailGenerator.generate(response);
+      let message={
+        from: process.env.EMAIL, // sender address
+        to: email, // list of receivers
+        subject: "Thanks for your interest ✔", // Subject line
+        // text: "successfully registered with us", // plain text body
+        html: mail, // html body
+      }
+
+        transporter.sendMail(message).then((info)=>{
+            return res.status(201).json({
+                msg:"email sent!",
+                info:info.messageId,
+                preview:nodemailer.getTestMessageUrl(info)
+            })
+         }).catch((e)=>{
+            res.status(500).json({e});
+         });
+
+
+         
 
 
        
